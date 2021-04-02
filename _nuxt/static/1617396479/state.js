24b92a71823d69b791621a045d5b5b022959a3d8
Dawntraoz <Dawntraoz@users.noreply.github.com>
2021-04-02 20:55:13 +0000

    deploy: 86dd16daa257c2eb8b06af33d0e4a0035d507c0a

--- _nuxt/static/1617396479/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FNMDLV-altura-tech\u002F_nuxt\u002Fstatic\u002F1617396479",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FNMDLV-altura-tech\u002F",assetsPath:"\u002FNMDLV-altura-tech\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1617396912/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FNMDLV-altura-tech\u002F_nuxt\u002Fstatic\u002F1617396912",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FNMDLV-altura-tech\u002F",assetsPath:"\u002FNMDLV-altura-tech\u002F_nuxt\u002F",cdnURL:a}}}}(null));
